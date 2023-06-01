f90e3fe1710a03e990fd12bed33869c2f044b622
kakhi1 <kmchedluri@gmail.com>
2023-06-02 00:06:35 +0400

    little changes

--- src/components/Homefooter.jsx
+++ src/components/Homefooter.jsx
@@ -1,7 +1,7 @@
 import { FiPlus } from "react-icons/fi";
 const Homefooter = () => {
   return (
-    <div className="h-[127px] md:h-[200px] bg-designColor flex items-center justify-center">
+    <div className="h-[127px] md:h-[200px] bg-designColor flex items-center justify-center border-x-4 border-black border-dashed">
       <div className="w-full h-full flex items-center justify-around text-white font-text lg:text-[80px] md:text-5xl text-3xl font-semibold">
         <h1>Collect your eco NTSs</h1>
         <h1></h1>
