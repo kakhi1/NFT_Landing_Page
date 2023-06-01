9e760a1a35a5b70f37daa47f7a8392a1cc0110d2
kakhi1 <kmchedluri@gmail.com>
2023-06-01 05:12:08 +0400

    finalsection  2

--- src/components/Finalsection.jsx
+++ src/components/Finalsection.jsx
@@ -1,53 +1,130 @@
-
-
 const Finalsection = () => {
   return (
-    <div className="font-text">
-        {/* ---------------1------------------ */}
-        <div className="w-full h-[1161px] border-black border-4 border-dashed flex items-center justify-center">
-            <div className="md:w-[75%] w-full md:ml-28 h-[892px] bg-finalsecbg bg-cover relative rounded-[60px] flex flex-col items-end ">
-                <div className="md:w-[504px] w-full h-[250px] z-30 md:absolute rounded-t-[60px] -left-[9%] bg-white top-[10%] flex flex-col ">
-                    <div className="w-full h-[10% ] flex items-start mb-5">
-                        <span className="w-[59px] h-[32px] rounded-lg bg-[#EFE4FC] flex  justify-center items-center">NFT</span>
-                    </div>
-                    <div className="w-full h-[38%] flex justify-start items-end">
-                        <div className="w-[8px] h-[80%] bg-[#6052FF] rounded-t-lg"></div>
-                        <h1 className="font-semibold md:text-7xl text-3xl ml-5">PFP NFT </h1>
-                        <div className="w-[39px]  h-[39px] rounded-full  bg-gradient-to-l from-[#DECAFF] to-[#691EE2] ml-20 mb-10"></div>
-                    </div>
-                    <div className="w-full h-[38%] flex justify-start items-start">
-                      <div className="w-[8px] h-[60%] bg-[#6052FF] rounded-b-lg"></div>
-                      <h1 className="font-semibold md:text-7xl text-3xl text-[#6052FF] ml-5">Marketplace</h1>
-                    </div>
-
-                </div>
-                <div className="md:w-[85%] w-full md:h-[70%] h-[50%] bg-finalsecbg2 bg-cover bg-top md:absolute -bottom-16 -left-[10%] z-30"></div>
-                <div className="md:w-[45%] w-full h-[25%] rounded-[60px] bg-white md:mr-[10%] md:top-0 bottom-0 flex flex-col z-30 justify-center items-start">                    
-                    <span className="font-medium md:text-lg text-sm text-left ml-7 ">We regulated NFT marketplace and payable in Cryptocurrencies and FIAT. You don’t have to be a computer geek to buy NFTs.</span>
-                    <button className="rounded-md w-[182px] h-[52px] ml-7 mt-2 text-white bg-[#6052FF] ">
-                    Discover now
-            </button>
-               </div>
-               <div className="w-[79px] h-[79px] rounded-full mt-[30%] mr-[15%] bg-gradient-to-l from-[#FFC56F] to-[#FF7051]"></div>
+    <div className="font-text ">
+      {/* ---------------1------------------ */}
+      <div className="w-full h-[1161px] border-black border-4 border-dashed flex items-center justify-center">
+        <div className="md:w-[75%] w-full md:ml-28 h-[892px] bg-finalsecbg bg-cover relative rounded-[60px] flex flex-col md:mb-14 items-end ">
+          <div className="md:w-[504px] w-full h-[250px] z-30 md:absolute rounded-t-[60px] -left-[9%] bg-white top-[10%] flex flex-col ">
+            <div className="w-full h-[10% ] flex items-start mb-5">
+              <span className="w-[59px] h-[32px] rounded-lg bg-[#EFE4FC] flex  justify-center items-center">
+                NFT
+              </span>
             </div>
-        </div>
-        {/* ---------------2------------------ */}
-        <div className="w-full h-[1839px] flex">
-          <div className="w-[75%] h-[50%]"></div>
-          <div className="w-[25%] h-[50%] flex flex-col m-16 gap-10">
-            <h1 className="w-[72px] h-[35px] text-3xl font-semibold "> Syne</h1>
-            <div className="w-full">
-              <div className="w-[305px] h-[325px] rounded-[28px] bg-black">
-              </div>
-              <div className="w-[244px] h-[320px] bg-red-400 "></div>
+            <div className="w-full h-[38%] flex justify-start items-end">
+              <div className="w-[8px] h-[80%] bg-[#6052FF] rounded-t-lg"></div>
+              <h1 className="font-semibold md:text-7xl text-3xl ml-5">
+                PFP NFT{" "}
+              </h1>
+              <div className="w-[39px]  h-[39px] rounded-full  bg-gradient-to-l from-[#DECAFF] to-[#691EE2] ml-20 mb-10"></div>
             </div>
-
+            <div className="w-full h-[38%] flex justify-start items-start">
+              <div className="w-[8px] h-[60%] bg-[#6052FF] rounded-b-lg"></div>
+              <h1 className="font-semibold md:text-7xl text-3xl text-[#6052FF] ml-5">
+                Marketplace
+              </h1>
+            </div>
           </div>
-
+          <div className="md:w-[85%] w-full md:h-[70%] h-[50%] bg-finalsecbg2 bg-cover bg-top md:absolute -bottom-16 -left-[10%] z-30"></div>
+          <div className="md:w-[45%] w-full h-[25%] rounded-[60px] bg-white md:mr-[10%] md:top-0 bottom-0 flex flex-col z-30 justify-center items-start">
+            <span className="font-medium md:text-lg text-sm text-left ml-7 ">
+              We regulated NFT marketplace and payable in Cryptocurrencies and
+              FIAT. You don’t have to be a computer geek to buy NFTs.
+            </span>
+            <button className="rounded-md w-[182px] h-[52px] ml-7 mt-2 text-white bg-[#6052FF] ">
+              Discover now
+            </button>
+          </div>
+          <div className="w-[79px] h-[79px] rounded-full mt-[30%] mr-[15%] bg-gradient-to-l from-[#FFC56F] to-[#FF7051]"></div>
         </div>
-
+      </div>
+      {/* ---------------2------------------ */}
+      <div className="w-full md:h-[1100px] lg:h-[920px] h-[1800px] flex md:grid grid-cols-4 grid-rows-3 flex-col  border-black border-x-4 border-dashed">
+        <div className="col-span-3 flex md:justify-center justify-start items-end max-md:h-[15%]">
+          <div className="relative flex flex-row w-[50%]">
+            <div className="w-[201px] h-[187px] rounded-3xl bg-[#E6F5D0] flex justify-center items-center">
+              <span className="font-semibold text-2xl">Gees</span>
+            </div>
+            <div className="w-[237px] h-[85px]  rounded-[32px] shadow-black drop-shadow-2xl lg:m-5 m-0 bg-[#FFFFFF] flex items-center justify-center absolute gap-3 top-[10%] md:left-[30%] left-[70%] z-30">
+              <div className="w-[50px] h-[50px] rounded-full bg-people bg-center bg-cover"></div>
+              <div className="w-[108px] h-[50px] text-left ">
+                <h1 className="text-[#342E2E] font-text font-semibold text-base">
+                  Anna May
+                </h1>
+                <span className="text-[#342E2E] font-text font-normal text-base">
+                  Donated $100
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row-span-3 w-full flex flex-col justify-start items-center max-md:h-[35%]">
+          <div className=" w-full flex flex-col  md:m-16  mx-0 md:gap-10 ">
+            <h1 className="w-[72px] h-[35px] text-3xl font-semibold "> Syne</h1>
+            <div className="w-full relative">
+              <div className="md:w-[90%] w-[300px] h-[302px] rounded-[28px] bg-finalsecbg3 bg-cover bg-center"></div>
+              <div className="md:w-[70%] w-[244px] h-[302px] p-5 md:gap-6 items-start absolute bg-[#EFE4FC] rounded-[18px] top-[80%]  md:left-[9%] left-[6%] flex flex-col">
+                <div className=" w-full flex flex-row justify-end">
+                  <span className="font-medium text-base">86%</span>
+                </div>
+                <span className="font-medium text-base">$350,000 USD</span>
+                <div className="w-[156px] h-[6px] bg-[#485BFF] flex justify-end">
+                  <div className="w-[30%] bg-white h-full"></div>
+                </div>
+                <span className="font-medium text-base">of 500,000</span>
+                <span className="font-regular text-xs text-left">
+                  Expanding internet connectivity with stratospheric balloons
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex items-center md:justify-center justify-start row-span-2 max-md:h-[25%]">
+          <div className="w-[244px] h-[350px]">
+            <div className="w-full h-full  relative flex flex-col justify-between items-center">
+              <h1 className="font-semibold text-2xl flex w-full items-start ml-1">
+                Luna
+              </h1>
+              <div className="w-full h-[288px] rounded-[28px] bg-finalsecbg4 bg-cover"></div>
+              <div className="w-[237px] h-[85px] rounded-[32px] shadow-black drop-shadow-2xl lg:m-5 m-0 bg-[#FFFFFF] flex items-center justify-center gap-3 absolute z-30 bottom-[16%] left-[80%]">
+                <div className="md:w-[50px] w-[30px] h-[50px]  rounded-full bg-people2 bg-center bg-cover"></div>
+                <div className="w-[108px] h-[50px] text-left ">
+                  <h1 className="text-[#342E2E] font-text font-semibold text-base ">
+                    Jane Holand
+                  </h1>
+                  <span className="text-[#342E2E] font-text font-normal text-base">
+                    Donated $500
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-span-2 row-span-2  flex flex-col bg-finalsecbg5 bg-cover bg-top max-md:h-[35%]">
+          <div className="flex flex-col gap-16 items-center">
+            <h1 className="font-semibold md:text-7xl text-2xl">
+              We are ecofriendly and a{" "}
+              <span className="text-[#FF7051]"> reliable</span>
+            </h1>
+            <span className="font-regular text-sm">
+              As an ambitious community-driven project, we've placed a strong
+              emphasis on setting the standard for unique NFT collector
+              experiences.{" "}
+            </span>
+            <div>
+              <button className="rounded-md w-[180px] h-[52px] text-white bg-[#6052FF] flex items-center justify-center">
+                Mint purpose
+              </button>
+            </div>
+          </div>
+          <div className="flex md:justify-end justify-center max-md:ml-20 max-md:mt-20">
+            <div className="w-[230px]  h-[187px] rounded-3xl bg-[#E7EEFA] flex justify-center items-center mr-[20%] md:mr-0">
+              <span className="font-semibold text-2xl">Sunport</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Finalsection
+export default Finalsection;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        text: ['Josefin Sans', "sans-serif"],
+        text: ["Josefin Sans", "sans-serif"],
       },
-      colors:{
+      colors: {
         designColor: "#5552FF",
       },
-      backgroundImage:{
-        hero:"url('./assets/Union.png')",
-        people:"url('./assets/people.jpg')",
-        people2:"url('./assets/people2.jpg')",
-        mintround1:"url('./assets/mintround1.jpg')",
-        mintround2:"url('./assets/mintround2.jpg')",
-        mintround3:"url('./assets/mintround3.jpg')",
-        mintround4:"url('./assets/mintround4.jpg')",
-        finalsecbg:"url('./assets/finalsecbg.jpg')",
-        finalsecbg2:"url('./assets/finalsecbg2.png')",
-        finalsecbg3:"url('./assets/finalsecbg3.jpg')",
-
-
-      }
+      backgroundImage: {
+        hero: "url('./assets/Union.png')",
+        people: "url('./assets/people.jpg')",
+        people2: "url('./assets/people2.jpg')",
+        mintround1: "url('./assets/mintround1.jpg')",
+        mintround2: "url('./assets/mintround2.jpg')",
+        mintround3: "url('./assets/mintround3.jpg')",
+        mintround4: "url('./assets/mintround4.jpg')",
+        finalsecbg: "url('./assets/finalsecbg.jpg')",
+        finalsecbg2: "url('./assets/finalsecbg2.png')",
+        finalsecbg3: "url('./assets/finalsecbg3.jpg')",
+        finalsecbg4: "url('./assets/finalsecbg4.png')",
+        finalsecbg5: "url('./assets/finalsecbg5.png')",
+      },
+      maxWidth: {
+        "screen-md": "768px",
+      },
     },
   },
   plugins: [],
-}
-
+};
